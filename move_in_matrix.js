2bb17f781e525ae005d689643b028bb73c807a4c
Alejandro Salas <asalas.wd@outlook.com>
2022-01-16 13:22:45 -0500

    add limits to movements

--- move_in_matrix.js
+++ move_in_matrix.js
@@ -8,10 +8,10 @@ function populateBoard(matrix,dimension){
   
   let line = 0;
 
-  for(let i=dimension; i>=0; i--){
+  for(let i=dimension-1; i>=0; i--){
     let tempArr = [];
     
-    for(let j=dimension; j>=0; j--){      
+    for(let j=dimension-1; j>=0; j--){      
       tempArr[j] = { x: j, y: i};      
     }    
     matrix[line] = tempArr;
@@ -41,38 +41,76 @@ function createPlayer() {
 
 let playerOne = createPlayer();
 
-function moveRight(player,board) {  
-  player.x++;
-  console.log("You moved right. You are now located at:")
-  console.log("X: " + player.x);
-  console.log("Y: " + player.y);
-  return player;
+function moveRight(player,board,dimension) {
+  if(player.x+1 == dimension){
+    console.log("You are at the limit. Choose another direction.");
+    console.log("You are located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+  } else {
+    player.x++;
+    console.log("You moved right. You are now located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+    return player;
+  }  
 }
 
-function moveLeft(player,board) {
-  player.x--;
-  console.log("You moved left. You are now located at:")
-  console.log("X: " + player.x);
-  console.log("Y: " + player.y);
-  return player;
+function moveLeft(player,board,dimension) {
+  if(player.x == 0){
+    console.log("You are at the limit. Choose another direction.");
+    console.log("You are located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+  } else {
+    player.x--;
+    console.log("You moved left. You are now located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+    return player;
+  }
 }
 
-function moveUp(player,board){
-  player.y++;
-  console.log("You moved up. You are now located at:")
-  console.log("X: " + player.x);
-  console.log("Y: " + player.y);
-  return player;
+function moveUp(player,board,dimension){
+  if(player.y+1 == dimension){
+    console.log("You are at the limit. Choose another direction.");
+    console.log("You are located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+  } else {
+    player.y++;
+    console.log("You moved up. You are now located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+    return player;
+  }
 }
 
-function moveDown(player,board){
-  player.y--;
-  console.log("You moved down. You are now located at:")
-  console.log("X: " + player.x);
-  console.log("Y: " + player.y);
-  return player;
+function moveDown(player,board,dimension){
+  if(player.y == 0){
+    console.log("You are at the limit. Choose another direction.");
+    console.log("You are located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+  } else {
+    player.y--;
+    console.log("You moved down. You are now located at:")
+    console.log("X: " + player.x);
+    console.log("Y: " + player.y);
+    return player;
+  }
 }
 
 console.log(playerOne);
-
-moveDown(playerOne,board);
+moveRight(playerOne,board,3);
+moveRight(playerOne,board,3);
+moveRight(playerOne,board,3);
+moveUp(playerOne,board,3);
+moveUp(playerOne,board,3);
+moveUp(playerOne,board,3);
+moveLeft(playerOne,board,3);
+moveLeft(playerOne,board,3);
+moveLeft(playerOne,board,3);
+moveDown(playerOne,board,3);
+moveDown(playerOne,board,3);
+moveDown(playerOne,board,3);
